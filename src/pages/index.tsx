3edac2ea9ccd8475510c3d60d3f4fbe8284ff105
yjnoh1016 <yjnoh1016@naver.com>
2021-01-25 13:48:00 +0900

    [FIX] Fix index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header } from "components";
-import Footer from "components/Footer/Footer";
 import StartPage1 from "components/StartPage1/StartPage1";
 import StartPage2 from "components/StartPage2/StartPage2";
 import StartPage3 from "components/StartPage3/StartPage3";
@@ -12,7 +11,6 @@ const index = () => {
       <StartPage1/>
       <StartPage2/>
       <StartPage3/>
-      <Footer/>
     </div>
   );
 };
