f21682c1a3c0dbc4e72d8d98c417f6b26cf6afa0
yjnoh1016 <yjnoh1016@naver.com>
2021-01-25 13:26:54 +0900

    [UPDATE] add Components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Header } from "components";
+import Footer from "components/Footer/Footer";
+import StartPage1 from "components/StartPage1/StartPage1";
+import StartPage2 from "components/StartPage2/StartPage2";
+import StartPage3 from "components/StartPage3/StartPage3";
 
 const index = () => {
   return (
     <div>
-      <Header />
+      <Header/>
+      <StartPage1/>
+      <StartPage2/>
+      <StartPage3/>
+      <Footer/>
     </div>
   );
 };
