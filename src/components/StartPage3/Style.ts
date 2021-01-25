9393a32d406bc165b39b0f318c01872b1d0afa35
yjnoh1016 <yjnoh1016@naver.com>
2021-01-25 13:25:54 +0900

    [CREATE] StartPage3 Component

---
+++ src/components/StartPage3/Style.ts
@@ -0,0 +1,60 @@
+import styled from "@emotion/styled";
+
+export const PageWrapper = styled.div `
+    position: absolute;
+    width: 100%;
+    left: 0px;
+    top: 2160px;
+
+    background: #FFFFFF;
+`;
+
+export const ContentWrapper = styled.div `
+    display: flex;
+    margin: 0 auto;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 1080px;
+`;
+
+export const FinalLogo = styled.div `
+    display: flex;
+
+    position: absolute;
+
+    left: 4%;
+    top: 173px;
+`;
+
+export const StartOlio = styled.div `
+    display: flex;
+
+    position: absolute;
+
+    left: 54%;
+    top: 462px;
+
+`;
+
+export const StartedBtn = styled.div `
+    display: flex;
+
+    position: absolute;
+
+    left: 54%;
+    top: 651px;
+
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+
+`;
+
+export const Vector7 = styled.div  `
+    display: flex;
+
+    position: absolute;
+    right: 0;
+    top: -381px;
+
+`;
