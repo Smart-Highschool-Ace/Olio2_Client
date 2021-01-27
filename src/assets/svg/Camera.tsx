d4831bed1be2f4048367cc4b7548ab5d06e3635a
iseolin76 <iseolin76@gmail.com>
2021-01-27 19:31:20 +0900

    Add svg for state=writing

---
+++ src/assets/svg/Camera.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+function Camera() {
+  return (
+    <svg
+      width={30}
+      height={30}
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        clipRule="evenodd"
+        d="M10 8.75l.925-1.387a2.5 2.5 0 012.08-1.113h3.99a2.5 2.5 0 012.08 1.113L20 8.75h6.25V22.5H3.75V8.75H10z"
+        stroke="#828282"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        clipRule="evenodd"
+        d="M15 18.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+        stroke="#828282"
+        strokeWidth={2}
+      />
+    </svg>
+  );
+}
+
+export default Camera;
---
+++ src/assets/svg/Search.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+function Search() {
+  return (
+    <svg
+      width={12}
+      height={12}
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g stroke="#828282">
+        <path d="M7.206 7.206L10 10" strokeLinecap="round" />
+        <path clipRule="evenodd" d="M5 8a3 3 0 100-6 3 3 0 000 6z" />
+      </g>
+    </svg>
+  );
+}
+
+export default Search;
