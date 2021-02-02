4944563dfef613e0cf6e7313214ba8b13cc79af9
dann1y <skypedanny@naver.com>
2021-02-02 20:19:19 +0900

    [UPDATE] Add Svg Path

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,6 @@
 export { default as Logo } from "./svg/Logo";
 export { default as Views } from "./svg/Views";
 export { default as Star } from "./svg/Star";
+export { default as Search } from "./svg/Search";
+export { default as ModalBanner } from "./svg/ModalBanner";
+export { default as Envelope } from "./svg/Envelope";
