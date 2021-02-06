5cbc6704066f2324b49afd1f452091fb04a44b45
dann1y <skypedanny@naver.com>
2021-02-06 20:19:19 +0900

    [UPDATE] Update Header Component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,37 @@
-import React from "react";
-import { Logo } from "assets";
+import React, { useCallback } from "react";
+import { Logo, Search } from "Assets";
 import * as S from "./Style";
 
+import { useModalContext } from "Utils/Contexts/ModalContext";
+import LoginModal from "Components/ModalList/Auth/LoginModal/LoginModal";
+
 const Header: React.FC = () => {
+  const { addModal } = useModalContext();
+
+  const handleClickLogin = useCallback(() => {
+    addModal({
+      title: "",
+      element: <LoginModal />,
+      showOnlyBody: true,
+      width: "1150px",
+      height: "697px",
+    });
+  }, []);
+
   return (
     <S.Positioner>
       <S.ContentWrapper>
         <S.Logo>
           <Logo height={47} />
         </S.Logo>
+        <S.SearchWrapper>
+          <S.SearchButton onClick={() => alert("검색입니다!")}>
+            <Search />
+          </S.SearchButton>
+          <S.SearchBar placeholder="검색어를 입력하세요" />
+        </S.SearchWrapper>
         <S.AuthWrapper>
-          <div>로그인</div>
+          <div onClick={() => handleClickLogin()}>로그인</div>
         </S.AuthWrapper>
       </S.ContentWrapper>
     </S.Positioner>
