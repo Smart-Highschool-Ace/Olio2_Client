0c44ad396045f8de399dbb3da3d5e22c9455d934
dann1y <skypedanny@naver.com>
2021-02-10 20:19:19 +0900

    [UPDATE] Add ModalConsumer & ModalProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
+import { Modal } from "Components";
 import type { AppProps } from "next/app";
 
+import { ModalProvider, ModalConsumer } from "Utils/Contexts/ModalContext";
+import ModalInfo from "Utils/Models/ModalInfo";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const renderModals = () => {
+    return (
+      <ModalConsumer>
+        {(state) =>
+          (state[0].modalList as ModalInfo[]).map((modal) => (
+            <Modal key={modal.id} info={modal} />
+          ))
+        }
+      </ModalConsumer>
+    );
+  };
+
+  return (
+    <ModalProvider>
+      <Component {...pageProps} />
+      {renderModals()}
+    </ModalProvider>
+  );
 }
 
 export default MyApp;
