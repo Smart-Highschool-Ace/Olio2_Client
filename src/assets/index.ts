3fdb32ae18cf73c5af633e73c3fa5fb318a94248
an0304073@naver.com <an0304073@naver.com>
2021-02-13 15:53:11 +0900

    [UPDATE] delete for fix

--- src/assets/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Logo } from "./svg/Logo";
-export { default as ProjectImg } from "./svg/ProjectImg";
-export { default as ProjectLogo } from "./svg/ProjectLogo";
-export { default as Clip } from "./svg/Clip";
-export { default as Mail } from "./svg/Mail";
-export { default as Camera } from "./svg/Camera";
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./Header/Header";
-export { default as Profile } from "./Profile/Profile";
-export { default as ProfileInfo } from "./ProfileInfo/ProfileInfo";
-export { default as ProfileModal } from "./Modal/ProfileModal/ProfileModal";
-export { default as TechStack } from "./TechStack/TechStack";
