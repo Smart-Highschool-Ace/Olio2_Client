8b302b0c730d91748c1b2d771661b20a5a801552
이서린 <iseolin76@gmail.com>
2021-02-19 23:16:35 +0900

    Update change the return value format for each Props value

--- src/components/project/ProjectModal.tsx
+++ src/components/project/ProjectModal.tsx
@@ -12,15 +12,28 @@ const ProjectModal: React.FC<ProjectModalProps> = (
   props: ProjectModalProps
 ) => {
   console.log(props.state);
-  return props.state === "writing" ? (
-    <Template toggle={props.toggle} width={1450} height={870}>
-      <ProjectModalWriting visible={props.visible} />
-    </Template>
-  ) : (
-    <Template toggle={props.toggle} width={1450} height={870}>
-      <ProjectModalEdit visible={props.visible} state={props.state} />
-    </Template>
-  );
+  return {
+    edit: (
+      <Template toggle={props.toggle} width={1450} height={1050}>
+        <ProjectModalEdit visible={props.visible} state={props.state} />
+      </Template>
+    ),
+    writing: (
+      <Template toggle={props.toggle} width={1450} height={1000}>
+        <ProjectModalWriting visible={props.visible} />
+      </Template>
+    ),
+    view: (
+      <Template toggle={props.toggle} width={1450} height={870}>
+        <ProjectModalEdit visible={props.visible} state={props.state} />
+      </Template>
+    ),
+    viewOnly: (
+      <Template toggle={props.toggle} width={1450} height={870}>
+        <ProjectModalEdit visible={props.visible} state={props.state} />
+      </Template>
+    ),
+  }[props.state];
 };
 
 export default ProjectModal;
