19c786578f0901c85e2bc86080580c50c5a69742
dann1y <skypedanny@naver.com>
2021-03-08 20:54:14 +0900

    [UPDATE] Add Components Path

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,1 +1,6 @@
 export { default as Header } from "./Header/Header";
+export { default as Board } from "./Board/Board";
+export { default as BoardItem } from "./BoardItem/BoardItem";
+export { default as Filter } from "./Filter/Filter";
+export { default as MainToggle } from "./MainToggle/MainToggle";
+export { default as Template } from "./Modal/Template/Template";
