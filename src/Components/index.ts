f2b7e082ab58117b86167effc5a7a3edb4e9b1d3
yjnoh1016 <yjnoh1016@naver.com>
2021-03-09 20:10:18 +0900

    [UPDATE] Components

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,7 @@
 export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
 export { default as StartPage } from "./StartPage/StartPage";
+export { default as MockupPage_project } from "./MockupPage_portfolio/MockupPage_portfolio";
+export { default as MockupPage_portfoilo } from "./MockupPage_project/Mockup_project";
+export { default as AboutPage } from "./AboutPage/AboutPage";
+export { default as LastPage } from "./LastPage/LastPage";
--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,21 +1,31 @@
-import styled from '@emotion/styled';
-import { Header, StartPage, Footer } from 'Components';
-import React from 'react';
+import styled from "@emotion/styled";
+import {
+  Header,
+  StartPage,
+  MockupPage_project,
+  MockupPage_portfoilo,
+  AboutPage,
+  LastPage,
+  Footer,
+} from "Components";
+import React from "react";
 
 const index: React.FC = () => {
-    return (
-        <Wrapper>
-            <Header/>
-            <StartPage/>
-            <Footer/>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Header />
+      <StartPage />
+      <MockupPage_portfoilo />
+      <MockupPage_project />
+      <AboutPage />
+      <LastPage />
+      <Footer/>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.div `
-    display: flex;
-    flex-direction: column;
+const Wrapper = styled.div`
+  background: url(/StartBackground.svg);
 `;
-
 
 export default index;
