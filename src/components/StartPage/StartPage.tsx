5574938f0e54e233854cfac36787b0d4818a87b8
yjnoh1016 <yjnoh1016@naver.com>
2021-03-09 20:10:01 +0900

    [UPDATE] StartPage Component

--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -1,98 +1,32 @@
-import { FinalLogo, PhoneImg, SS, StartImg, StartProfile, TS, WS } from 'Assets';
-import React from 'react';
+import { StartImg } from "Assets";
+import React from "react";
 import * as S from "./Style";
 
 const StartPage: React.FC = () => {
-    return (
-        <S.Positioner>
-            <S.Wrapper>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;700&display=swap" rel="stylesheet"></link>
-            <S.StartImg>
-                <StartImg/>
-            </S.StartImg>
-            <S.SMH>Software Meister Highschool</S.SMH>
-            <S.SMYP>Show me your PortF<S.Olio1>olio</S.Olio1></S.SMYP>
-            <S.Info>
-                소프트웨어마이스터고등학교 학생들의 <br/>
-                포트폴리오를 모아놓은 포트폴리오 웹 서비스입니다.
-            </S.Info>
-            <S.StartedBtn1>
-                Let's Started
-            </S.StartedBtn1>
-            <S.Explain1>
-                “ 잘 쓴 포트폴리오를 한 눈에 보기 쉽게 ”
-            </S.Explain1>
-            <S.Rec1></S.Rec1>
-            <S.ExAbout1>
-                지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다.지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다. 닭발 요즘 안땡김 그래도 먹고싶음
-            </S.ExAbout1>
-            <S.ProfileImg>
-                <StartProfile/>
-            </S.ProfileImg>
-            <S.Explain2>
-                “ 웹 뿐만아니라 앱에서도 가능 ! ”
-            </S.Explain2>
-            <S.Rec2></S.Rec2>
-            <S.ExAbout2>
-                지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다.지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다.지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다.지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다.지금 너무너무 배가고파서 뒤질 것 같다. 그래서 갈비탕을 먹을거다.
-            </S.ExAbout2>
-            <S.PhoneImg>
-                <PhoneImg/>
-            </S.PhoneImg>
-            <S.MiniSection>
-                <S.MiniTitle>
-                    OLIO
-                </S.MiniTitle>
-                <S.MiniSub>
-                    소프트웨어마이스터고등학교 포트폴리오 웹 서비스
-                </S.MiniSub>
-                <S.MiniInfo>
-                    기존에 있던 ‘드림북'을 대체하기 위한<br/>
-                    포트폴리오 웹 서비스 입니다.
-                </S.MiniInfo>
-            </S.MiniSection>
-            <S.About>
-                <S.WS>
-                    <WS/>
-                    <h5>공간제약이 없는 웹 서비스</h5>
-                    <p>
-                        책으로 이루어진 ‘드림북'을 대체한 웹서비스로 <br/>
-                        공간의 제약을 받지 않고 열람 가능
-                    </p>
-                </S.WS>
-                <S.TS>
-                    <TS/>
-                    <h5>기업의 채용을 생각한 서비스</h5>
-                    <p>
-                        다양한 학생들의 포트폴리오를 <br/>
-                        한눈에 보기쉽게
-                    </p>
-                </S.TS>
-                <S.SS>
-                    <SS/>
-                    <h5>학생을 생각한 서비스</h5>
-                    <p>
-                        웹으로 자신의 포트폴리오 정리와 <br/>
-                        선배나 친구들의 프로젝트 열람 가능
-                    </p>
-                </S.SS>
-            </S.About>
-            <S.StartOlio>
-                쉽고 간편한 포트폴리오 웹 서비스<br/>
-                <p><S.Apostrophe>‘ <S.Olio2>olio</S.Olio2> ’</S.Apostrophe> 시작하세요</p>
-            </S.StartOlio>
-            <S.StartedBtn3>
-                Let's Started
-            </S.StartedBtn3>
-            <S.FinalLogo>
-                    <FinalLogo/>
-            </S.FinalLogo>
-            </S.Wrapper>
-        </S.Positioner>
-    );
+  return (
+    <S.Positioner>
+      <S.Wrapper>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <S.StartImg>
+          <StartImg />
+        </S.StartImg>
+        <S.SMH>Software Meister Highschool</S.SMH>
+        <S.SMYP>
+          Show me your PortF<S.Olio1>olio</S.Olio1>
+        </S.SMYP>
+        <S.Info>
+          소프트웨어마이스터고등학교 학생들의 <br />
+          포트폴리오를 모아놓은 포트폴리오 웹 서비스입니다.
+        </S.Info>
+        <S.StartedBtn1>Let's Started</S.StartedBtn1>
+      </S.Wrapper>
+    </S.Positioner>
+  );
 };
 
-
-
 export default StartPage;
+
--- src/components/StartPage/Style.ts
+++ src/components/StartPage/Style.ts
@@ -1,432 +1,82 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
-export const Positioner = styled.div `
-    position: relative;
-
-    background: url(/StartBackground.svg);
-
-    display: flex;
-
-    width: 100%;
-    padding-bottom: 17vh;
+export const Positioner = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
 `;
 
-export const Wrapper = styled.div `
-    position: relative;
-    width: 100%;
+export const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
 `;
 
-export const StartImg = styled.span `
-    display: flex;
-    margin-top: 46.7vh;
-    margin-left: 61.45vw;
+export const StartImg = styled.span`
+  display: flex;
+  margin-top: 46.7vh;
+  margin-left: 61.45vw;
 `;
 
-export const SMH = styled.span `
-    font-family: Josefin Sans;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 50px;
-    line-height: 50px;
-
-    color: #000000;
-
-    display: flex;
-
-    margin-left: 13vw;
-    margin-top: -54.7vh;
+export const SMH = styled.span`
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 50px;
+  color: #000000;
+  display: flex;
+  margin-left: 13vw;
+  margin-top: -54.7vh;
 `;
 
-export const SMYP = styled.span `
-    font-family: Josefin Sans;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 50px;
-    line-height: 50px;
-
-    color: #000000;
-
-    display: flex;
-
-    margin-left: 13vw;
-    margin-top: 2vh;
-
+export const SMYP = styled.span`
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 50px;
+  color: #000000;
+  display: flex;
+  margin-left: 13vw;
+  margin-top: 2vh;
 `;
 
-export const Olio1 = styled.span `
-    background: linear-gradient(to bottom, rgba(109, 173, 168, 1), rgba(226, 234, 206, 1)); 
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+export const Olio1 = styled.span`
+  background: linear-gradient(
+    to bottom,
+    rgba(109, 173, 168, 1),
+    rgba(226, 234, 206, 1)
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
-export const Info = styled.span `
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: 100;
-    font-size: 25px;
-    line-height: 30px;
-
-    color: #000000;
-
-    display: flex;
-    margin-left: 13vw;
-    margin-top: 4vh;
+export const Info = styled.span`
+  font-family: Apple SD Gothic Neo;
+  font-style: normal;
+  font-weight: 100;
+  font-size: 25px;
+  line-height: 30px;
+  color: #000000;
+  display: flex;
+  margin-left: 13vw;
+  margin-top: 4vh;
 `;
 
-export const StartedBtn1 = styled.button `
-    width: 207px;
-    height: 69px;
-    background: #6E8783;
-    border-radius: 10px;
-
-    border: 0;
-
-    font-family: Josefin Sans;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 20px;
-    line-height: 20px;
-
-    text-align: center;
-
-    color: #FFFFFF;
-
-    margin-left: 13vw;
-    margin-top: 9.8vh;
+export const StartedBtn1 = styled.button`
+  width: 207px;
+  height: 69px;
+  background: #6e8783;
+  border-radius: 10px;
+  border: 0;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 20px;
+  text-align: center;
+  color: #ffffff;
+  margin-left: 13vw;
+  margin-top: 9.8vh;
 `;
-
-export const Explain1 = styled.span `
-    width: 797px;
-
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 50px;
-    line-height: 60px;
-
-    text-align: center;
-
-    color: #000000;
-
-    display: flex;
-    margin-left: 7.7vw;
-    margin-top: 48vh;
-`;
-
-export const Rec1 = styled.div `
-    width: 731px;
-    height: 39px;
-
-    background: rgba(213, 234, 206, 0.54);
-
-    display: flex;
-    margin-left: 11.67vw;
-    margin-top: -0.8vh;
-`;
-
-export const ExAbout1 = styled.div `
-    width: 669px;
-
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 20px;
-    line-height: 24px;
-    text-align: center;
-
-    color: #000000;
-
-    display: flex;
-    margin-top: 3.1vh;
-    margin-left: 13.23vw;
-`;
-
-export const ProfileImg = styled.div `
-
-    display: flex;
-    margin-top: 9vh;
-    margin-left: 18vw;
-`;
-
-export const Explain2 = styled.span `
-    width: 655px;
-
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 50px;
-    line-height: 60px;
-
-    text-align: center;
-
-    color: #000000;
-
-    display: flex;
-    margin-top: 22.3vh;
-    margin-left: 11.6vw;
-`;
-
-export const Rec2 = styled.div `
-    width: 669px;
-    height: 39px;
-
-    background: rgba(213, 234, 206, 0.54);
-
-    display: flex;
-    margin-left: 11.61vw;
-    margin-top: -2.6vh;
-`;
-
-export const ExAbout2 = styled.div `
-    width: 669px;
-
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 20px;
-    line-height: 24px;
-    text-align: center;
-
-    color: #000000;
-
-    display: flex;
-    margin-top: 4.33vh;
-    margin-left: 11.6vw;
-`;
-
-export const PhoneImg = styled.div `
-
-    display: flex;
-    margin-top: 10.3vh;
-    margin-left: 45.9vw;
-`;
-
-export const MiniSection = styled.div `
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    margin-top: 8.2vh;
-`;
-
-export const MiniTitle = styled.div `
-
-    width: 140px;
-    height: 40px;
-
-    background: rgba(90, 148, 139, 0.27);
-    border-radius: 30px;
-
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: 800;
-    font-size: 20px;
-    line-height: 24px;
-    
-    text-align: center;
-    padding-top: 1.1vh;
-
-    color: rgba(90, 148, 139, 0.77);
-`;
-
-export const MiniSub = styled.div `
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 30px;
-    line-height: 36px;
-
-    color: #000000;
-
-    margin-top: 2.3vh;
-`;
-
-export const MiniInfo = styled.div `
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: 100;
-    font-size: 20px;
-    line-height: 24px;
-    text-align: center;
-
-    color: #000000;
-
-    margin-top: 2.9vh;
-`;
-
-export const About = styled.div `
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    margin-top: 10.8vh;
-`;
-
-export const WS = styled.div `
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    h5 {
-        font-family: Apple SD Gothic Neo;
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-
-        text-align: center;
-
-        color: #000000;
-
-        margin-top: 6.6vh;
-    }
-
-    p {
-        font-family: Apple SD Gothic Neo;
-        font-style: normal;
-        font-weight: 200;
-        font-size: 16px;
-        line-height: 19px;
-        text-align: center;
-
-        color: #000000;
-
-        margin-top: 1.7vh;
-    }
-`; 
-
-export const TS = styled.div `
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    margin-left: 7.1vw;
-    margin-right: 8.8vw;
-
-    h5 {
-        font-family: Apple SD Gothic Neo;
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-        
-        text-align: center;
-
-        color: #000000;
-
-        margin-top: 6.6vh;
-    }
-
-    p {
-        font-family: Apple SD Gothic Neo;
-        font-style: normal;
-        font-weight: 200;
-        font-size: 16px;
-        line-height: 19px;
-        text-align: center;
-
-        color: #000000;
-
-        margin-top: 1.7vh;
-    }
-`; 
-
-export const SS = styled.div `
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    h5 {
-        font-family: Apple SD Gothic Neo;
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-        
-        text-align: center;
-
-        color: #000000;
-
-        margin-top: 6.6vh;
-    }
-
-    p {
-        font-family: Apple SD Gothic Neo;
-        font-style: normal;
-        font-weight: 200;
-        font-size: 16px;
-        line-height: 19px;
-        text-align: center;
-
-        color: #000000;
-
-        margin-top: 1.7vh;
-    }
-`; 
-
-export const StartOlio = styled.span `
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: 100;
-    font-size: 40px;
-    line-height: 48px;
-
-    color: #000000;
-
-    display: flex;
-    flex-direction: column;
-    margin-left: 58.2vw;
-    margin-top: 72vh;
-`;
-
-export const Apostrophe = styled.span `
-    font-size: 80px;
-    line-height: 96px;
-
-    color: #000000;
-`
-
-export const Olio2 = styled.span `
-    font-family: Josefin Sans;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 80px;
-    line-height: 80px;
-    background: linear-gradient(to bottom, rgba(90, 148, 139, 1), rgba(226, 234, 206, 1)); 
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-`
-
-export const StartedBtn3 = styled.button `
-    position: inline-block;
-    width: 200px;
-    height: 70px;
-    background: #FFFFFF;
-    border: 3px solid #5A948B;
-    box-sizing: border-box;
-    border-radius: 10px;
-
-    font-family: Apple SD Gothic Neo;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 20px;
-    line-height: 24px;
-
-    justify-content: center;
-    align-items: center;
-
-    color: #5A948B;
-
-    display: flex;
-    margin-left: 58.2vw;
-    margin-top: 1.2vh;
-`;
-
-export const FinalLogo = styled.span `
-    display: flex;
-    margin-left: 4.25vw;
-    margin-top: -60vh;
-`;
-
-
 
