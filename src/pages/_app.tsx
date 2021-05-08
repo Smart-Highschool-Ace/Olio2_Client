07cc4dfe6c5d1332e64fc4bbadb2e42ae9093eb8
dann1y <skypedanny@naver.com>
2021-05-08 20:13:19 +0900

    modify : apply apollo-client context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,36 @@
 import { Modal } from "Components";
 import type { AppProps } from "next/app";
-import { Fragment } from "react";
 import GlobalStyle from "Styles/GlobalStyle";
 import { Global } from "@emotion/react";
-
 import { ModalProvider, ModalConsumer } from "Utils/Contexts/ModalContext";
 import ModalInfo from "Utils/Models/ModalInfo";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token && `Bearer ${token}`,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
+
+const client = new ApolloClient({
+  link: link as any,
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const renderModals = () => {
@@ -21,11 +46,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   };
 
   return (
-    <ModalProvider>
-      <Global styles={GlobalStyle} />
-      <Component {...pageProps} />
-      {renderModals()}
-    </ModalProvider>
+    <ApolloProvider client={client}>
+      <ModalProvider>
+        <Global styles={GlobalStyle} />
+        <Component {...pageProps} />
+        {renderModals()}
+      </ModalProvider>
+    </ApolloProvider>
   );
 }
 
