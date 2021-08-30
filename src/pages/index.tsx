11501f3773b44732d7435d25c6c84640673b70b4
dann1y <skypedanny@naver.com>
2021-08-30 10:00:52 +0900

    modify : apply <Head>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import Head from "next/head";
+
 import {
   Header,
   StartPage,
@@ -11,14 +13,21 @@ import React from "react";
 
 const index: React.FC = () => {
   return (
-    <Wrapper>
-      <Header />
-      <StartPage />
-      <MockupPage_portfoilo />
-      <MockupPage_project />
-      <AboutPage />
-      <LastPage />
-    </Wrapper>
+    <div>
+      <Head>
+        <title>Olio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Wrapper>
+        <Header />
+        <StartPage />
+        <MockupPage_portfoilo />
+        <MockupPage_project />
+        <AboutPage />
+        <LastPage />
+      </Wrapper>
+    </div>
   );
 };
 
