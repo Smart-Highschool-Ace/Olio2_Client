ad8de2363da9142c3e71a61c403b48b36e9d1758
iseolin76 <iseolin76@gmail.com>
2022-05-24 18:06:32 +0900

    [UPDATE] REACT_APP -> NEXT_PUBLIC

--- src/lib/withApollo.tsx
+++ src/lib/withApollo.tsx
@@ -7,7 +7,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: process.env.GQL_ENDPOINT,
+  uri: process.env.NEXT_PUBLIC_GQL_ENDPOINT,
 });
 
 const authLink = setContext((_, { headers }) => {
