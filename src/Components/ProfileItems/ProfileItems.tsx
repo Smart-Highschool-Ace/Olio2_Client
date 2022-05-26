8b1fecba1a43193fb74eada1395a736544c2236f
sunrabbit123 <qudwls185@naver.com>
2022-05-27 08:22:42 +0900

    fix : Do not use Array index in keys

--- src/Components/ProfileItems/ProfileItems.tsx
+++ src/Components/ProfileItems/ProfileItems.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import * as S from "./Style";
 
 type MyProject = {
+  project_id: number;
   name: string;
   icon: string;
   description: string;
@@ -36,8 +37,8 @@ const ProfileItems: React.FC<ProfileItemProps> = (props: ProfileItemProps) => {
           <span>프로젝트</span>
         </S.ContentTitle>
         <S.ProjectContent>
-          {ProjectDummyData.map((data, idx) => (
-            <S.ProjectWrapper key={idx}>
+          {ProjectDummyData.map(data => (
+            <S.ProjectWrapper key={data.project_id}>
               <Image src={data.icon} width="auto" height="auto" />
               <S.ProjectTitle>
                 <span>{data.name}</span>
@@ -53,8 +54,8 @@ const ProfileItems: React.FC<ProfileItemProps> = (props: ProfileItemProps) => {
           <span>수상경력</span>
         </S.ContentTitle>
         <S.EtcContent>
-          {AwardDummyData.map((data, idx) => (
-            <S.EtcWrapper key={idx}>
+          {AwardDummyData.map(data => (
+            <S.EtcWrapper key={data.name}>
               <span>{data.name}</span>
               <div>{data.organization}</div>
               <div>{data.date}</div>
@@ -70,8 +71,8 @@ const ProfileItems: React.FC<ProfileItemProps> = (props: ProfileItemProps) => {
           <span>자격증</span>
         </S.ContentTitle>
         <S.EtcContent>
-          {CertificateDummyData.map((data, idx) => (
-            <S.EtcWrapper key={idx}>
+          {CertificateDummyData.map(data => (
+            <S.EtcWrapper key={data.name}>
               <span>{data.name}</span>
               <div>{data.organization}</div>
               <div>{data.date}</div>
