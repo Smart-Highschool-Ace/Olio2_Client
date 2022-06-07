33b3d3327089b94908130252c70ea1629cb88d4f
iseolin76 <iseolin76@gmail.com>
2022-06-07 14:55:28 +0900

    [UPDATE] switch case -> object

--- src/lib/findModal.tsx
+++ src/lib/findModal.tsx
@@ -12,29 +12,15 @@ const findModal = (
   modalName: ModalName,
   state: ModalStateType
 ): JSX.Element => {
-  let modal: JSX.Element = <></>;
+  const findModalObject = {
+    Project: <ProjectModal state={state} />,
+    Login: <LoginModal />,
+    FieldChoice: <FieldChoice state={state} />,
+    Register: <RegisterModal />,
+    EmailConfirm: <EmailConfirm />,
+  };
 
-  switch (modalName) {
-    case "Project":
-      modal = <ProjectModal state={state} />;
-      break;
-    case "Login":
-      modal = <LoginModal />;
-      break;
-    case "FieldChoice":
-      modal = <FieldChoice state={state} />;
-      break;
-    case "Register":
-      modal = <RegisterModal />;
-      break;
-    case "EmailConfirm":
-      modal = <EmailConfirm />;
-      break;
-    default:
-      return <>잘못된 modal name</>;
-  }
-
-  return modal;
+  return <>{findModalObject[modalName]}</>;
 };
 
 export default findModal;
