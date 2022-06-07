ab0c233ebc56336c9ff90df30328a7614fe527e5
iseolin76 <iseolin76@gmail.com>
2022-06-07 12:38:30 +0900

    [DELETE] Project comment

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Project } from "../Components/index";
 
 const project: React.FC = () => {
-  // {/* /* visible은 모달이 보이는 지 안 보이는 지, state는 모달 상태
-  // (view, viewOnly, edit, writing) */ */}
   return (
     <>
       <div>어쨌든 프로젝트 페이지</div>
