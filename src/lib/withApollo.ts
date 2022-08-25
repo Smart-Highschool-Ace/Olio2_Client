82e1d39e207771fe59409a070e9a8aaa6053c2d8
iseolin76 <iseolin76@gmail.com>
2022-08-25 12:01:26 +0900

    [DELETE] unuse code

--- src/lib/withApollo.ts
+++ src/lib/withApollo.ts
@@ -1,26 +1,8 @@
 import {
   ApolloClient,
-  createHttpLink,
   InMemoryCache,
   NormalizedCacheObject,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-
-const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GQL_ENDPOINT,
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   ssrMode: true,
